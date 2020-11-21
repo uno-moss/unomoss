0ac0f9239d82b450c44c4a43040831b1ba6463d0
Brooke Luro <brookeluro@Brookes-MacBook-Pro.local>
2020-11-20 19:40:23 -0500

    profile page changes

--- client/components/Greenhouse/Greenhouse.js
+++ client/components/Greenhouse/Greenhouse.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const Greenhouse = () => {
+  // plants will be an array of plants
+  const [plants, setPlants] = useState([]);
+
+  // on mount: fetch user's collection of plants from the db
+  useEffect(() => {
+    fetch('/').
+  }, [])
   return (
     <div>
       Greenhouse page
