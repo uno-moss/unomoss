cb7a6651a35f115ef29e61830b6400192458a19c
Brooke Luro <brookeluro@Brookes-MacBook-Pro.local>
2020-11-20 23:02:53 -0500

    greenhouse page changes

--- client/components/Greenhouse/Greenhouse.js
+++ client/components/Greenhouse/Greenhouse.js
@@ -1,21 +1,21 @@
+/* eslint-disable react/jsx-filename-extension */
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import './Greenhouse.scss';
+import AllRows from './AllPlants';
+import SinglePlant from './PlantItem';
+
+const url = 'https://image-server-codesmith.firebaseapp.com/images';
+//import Plant from "./components/Plant/Plant";
+//import SearchPlant from "./components/SearchPlant/SearchPlant";
 
 const Greenhouse = () => {
   // plants will be an array of plants
-  const [plants, setPlants] = useState([]);
-
+  //const [plants, setPlants] = useState([]);
   // on mount: fetch user's collection of plants from the db
-  useEffect(() => {
-    fetch('/').
-  }, [])
-  return (
-    <div>
-      Greenhouse page
-      <Link to="/plant">Go to plant</Link>
-      <Link to="/search">Go to Search</Link>
-    </div>
-  );
+  //useEffect(() => {}, []);
+  // {plants}
+  return <AllRows url={url} />;
 };
 
 export default Greenhouse;
